96d4be37d9a19369983ebb2400976a7cab319684
spaylor4 <spaylor4@yahoo.com>
2020-07-19 12:03:12 -0400

    Create helper function to find mid index

---
+++ cs5010-prog-sys/mod4-bin-search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module 4 Exercise: Binary Search
+Shannon Paylor
+sep4hy
+"""
+
+import math
+
+def getMid(start_ind, end_ind):
+    spread = end_ind - start_ind
+    mid = math.floor(spread/2)
+    print(F"Mid is: {mid}")
+    return mid
+
+def binarySearch(item_list,target):
+    search_min = 0
+    search_max = len(item_list) - 1
+    
+    
+    
+    
+    
+    
+
+
+item_list = [1, 2, 4, 6, 7, 9]
+
+
