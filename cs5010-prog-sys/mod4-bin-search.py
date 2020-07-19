6469199592618dfe51c96052d26c527bfc3b5a0f
spaylor4 <spaylor4@yahoo.com>
2020-07-19 12:19:39 -0400

    Fix getMid func for nonzero start

--- cs5010-prog-sys/mod4-bin-search.py
+++ cs5010-prog-sys/mod4-bin-search.py
@@ -10,7 +10,7 @@ import math
 
 def getMid(start_ind, end_ind):
     spread = end_ind - start_ind
-    mid = math.floor(spread/2)
+    mid = math.floor(spread/2) + start_ind
     print(F"Mid is: {mid}")
     return mid
 
@@ -21,18 +21,20 @@ def binarySearch(item_list,target):
     if len(item_list) == 0:
         return False #target can't be in an empty list
     
-    while search_max >= search_min:
+    while search_max > search_min:
         mid = getMid(search_min, search_max)
         if target == item_list[mid]:
-            return True #if target equals mid element, we can stop searching
+            print("true")
+            # return True #if target equals mid element, we can stop searching
         elif target < item_list[mid]:
             search_max = mid - 1 #search bottom half in next iteration
         else:
             search_min = mid + 1 #search top half in next iteration
     
-    
+    return False #will only get here if while loop never returns True    
 
 
 item_list = [1, 2, 4, 6, 7, 9]
 
-getMid(0, 0)
+binarySearch(item_list, 6)
+target = 6
