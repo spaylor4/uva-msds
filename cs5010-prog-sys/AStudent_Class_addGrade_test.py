3cc83762f628939264f837310d3ce8ee94c5137e
spaylor4 <spaylor4@yahoo.com>
2020-07-21 13:48:23 -0400

    Create unit test for addGrade method

--- cs5010-prog-sys/AStudent_Class_addGrade_test.py
+++ cs5010-prog-sys/AStudent_Class_addGrade_test.py
@@ -11,9 +11,15 @@ from AStudent_Class import AStudent
 
 class StudentTestCase(unittest.TestCase):
     def test_add_grade(self):
+        #add grade method should take a given grade and append it to the student's list of grades
         #instantiate student and perform operations
+        test_student = AStudent("Test Student", "1234", 2, courses = ["Stats", "CompSci"])
+        test_student.addGrade(90)
+        test_student.addGrade(85)
+        test_student.addGrade(87)
         
         #assert expected vs actual
-        # self.assertEqual(actual, expected)
+        self.assertEqual(test_student.grades, [90, 85, 87])
 
-
+if __name__ == '__main__':
+    unittest.main()
