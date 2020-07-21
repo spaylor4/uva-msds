ea936a82b3e71f574376c59589bca6b1ead7cdbe
spaylor4 <spaylor4@yahoo.com>
2020-07-21 13:38:57 -0400

    Setup unit test file

---
+++ cs5010-prog-sys/AStudent_Class_addGrade_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module 5 Exercise: Python Unit Testing
+Shannon Paylor
+sep4hy
+"""
+
+import unittest
+from AStudent_Class import AStudent
+
+class StudentTestCase(unittest.TestCase):
+    def test_add_grade(self):
+        #instantiate student and perform operations
+        
+        #assert expected vs actual
+        # self.assertEqual(actual, expected)
+
+
