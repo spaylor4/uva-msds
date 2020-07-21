e590955fce906d8f68f4267fae14f3529a0e4c7b
spaylor4 <spaylor4@yahoo.com>
2020-07-21 13:56:10 -0400

    Write unit test for average grade method

--- cs5010-prog-sys/AStudent_Class_addGrade_test.py
+++ cs5010-prog-sys/AStudent_Class_addGrade_test.py
@@ -20,6 +20,22 @@ class StudentTestCase(unittest.TestCase):
         
         #assert expected vs actual
         self.assertEqual(test_student.grades, [90, 85, 87])
+        
+    def test_average_grade(self):
+        #average method should return the average of all of the student's grades
+        #instantiate student and perform operations
+        test_student = AStudent("Test Student", "1234", 2, courses = ["Stats", "CompSci"])
+        test_student.addGrade(90)
+        test_student.addGrade(85)
+        test_student.addGrade(87)
+        
+        test_student_avg = test_student.average()
+        
+        #assert expected vs actual
+        expected_val = (90 + 85 + 87) / 3
+        self.assertEqual(test_student_avg, expected_val)
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
