f91aa981c198837dda9ca3a00f1b1d4b604919a5
spaylor4 <spaylor4@yahoo.com>
2020-07-22 11:37:48 -0400

    Extract clean text for desired elements

--- cs5010-prog-sys/hw-web-scrape.py
+++ cs5010-prog-sys/hw-web-scrape.py
@@ -18,11 +18,19 @@ soup = BeautifulSoup(r.text)
 tickers = soup.findAll("span", class_ = "ticker", attrs={'data-id': False})
 #tickers show up twice: in article text and in related tickers sidebar; data-id present in p but absent in table
 
+for ticker in tickers:
+    tckr = ticker.findAll("a")[0]
+    quote_link = tckr.get("href")
+    company = tckr.get("title")
+    symbol = ticker.findAll("span", class_ = "symbol")[0].contents[0]
 
-
-title = soup.findAll("h1")
+title = soup.find("h1").contents[0]
 
 pub_dt = soup.findAll("div", "publication-date")
+pub_dt = pub_dt[0].find("svg").next_sibling.strip()
 
 author = soup.findAll("div", "author-name")
+author_name = author[0].findAll("a")[0].contents[0]
+author_usrnm = author[0].findAll("div", class_ = "author-username")[0].contents[0]
+author_usrnm = author_usrnm.strip().replace("(", "").replace(")", "")
 
